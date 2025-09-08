4712d0243d1b8370c063b99edc6dac62412ebd88
Sofiman <sofiane.meftah@outlook.fr>
2025-09-09 00:16:28 +0200

    feat(sht4x): add finite I2C timeout

--- components/sht4x/sht4x.c
+++ components/sht4x/sht4x.c
@@ -11,12 +11,16 @@
 #define sht4x_delay_us(Us) ulp_lp_core_delay_us((Us) + 10)
 #endif
 
+#ifndef SHT4x_I2C_TIMEOUT
+#define SHT4x_I2C_TIMEOUT 5000 // TODO: Unit is different depending SHT4x_LP_CORE_I2C. Clock cycles vs ms
+#endif // !SHT4x_I2C_TIMEOUT
+
 sht4x_result_t sht4x_cmd_(sht4x_handle_t handle, sht4x_cmd_t cmd, uint16_t cmd_max_duration_us)
 {
     sht4x_result_t res = { 0 };
 
     _Static_assert(sizeof(cmd) == 1);
-    res.err = sht4x_i2c_write(handle, &cmd, sizeof(cmd), -1);
+    res.err = sht4x_i2c_write(handle, &cmd, sizeof(cmd), SHT4x_I2C_TIMEOUT);
     if (res.err != ESP_OK) return res;
 
     if (cmd_max_duration_us > 0)
@@ -25,7 +29,7 @@ sht4x_result_t sht4x_cmd_(sht4x_handle_t handle, sht4x_cmd_t cmd, uint16_t cmd_m
     if (cmd == SHT4x_SOFT_RESET) return res;
 
     _Static_assert(sizeof(res.frames) == 6, "SHT4x frame is always 6 bytes");
-    res.err = sht4x_i2c_read(handle, (uint8_t*)&res.frames, sizeof(res.frames), -1);
+    res.err = sht4x_i2c_read(handle, (uint8_t*)&res.frames, sizeof(res.frames), SHT4x_I2C_TIMEOUT);
     if (res.err != ESP_OK) return res;
 
     if (sensirion_common_calculate_crc8(res.frames[0]) != res.frames[0].crc) res.err = ESP_ERR_INVALID_CRC;
